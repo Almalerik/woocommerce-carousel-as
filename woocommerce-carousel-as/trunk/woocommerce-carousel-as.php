786416ed73492daa9d7754a3e174d4df8f6e289b
Almalerik <almalerik@gmail.com>
2014-10-24 12:32:28 +0200

    Initial commit

---
+++ woocommerce-carousel-as/trunk/woocommerce-carousel-as.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress WooCommerce Carousel
+ * Plugin URI:        http://almalerik.github.io/woocommerce-carousel-as
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress dashboard.
+ * Version:           1.0.0
+ * Author:            Almalerik
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       woocommerce-carousel-as
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-carousel-as-activator.php';
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-carousel-as-deactivator.php';
+
+/** This action is documented in includes/class-woocommerce-carousel-as-activator.php */
+register_activation_hook( __FILE__, array( 'Plugin_Name_Activator', 'activate' ) );
+
+/** This action is documented in includes/class-woocommerce-carousel-as-deactivator.php */
+register_deactivation_hook( __FILE__, array( 'Plugin_Name_Deactivator', 'deactivate' ) );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-woocommerce-carousel-as.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
+}
+run_plugin_name();
